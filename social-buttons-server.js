6de45bd3aba30f700ac43b34d334211cea3575b1
Zaki Manian <zaki@manian.org>
2014-01-11 22:24:43 -0800

    Change the port in production

--- social-buttons-server.js
+++ social-buttons-server.js
@@ -71,7 +71,7 @@ app.get('/', function(req, res) {
   });
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3145;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
